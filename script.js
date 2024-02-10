533860e3dea78d2739c3e87da2658a678eb5c879
Pedro Mendez <tostado_mt@hotmail.com>
2024-02-10 17:36:43 -0600

    finalSwift

--- script.js
+++ script.js
@@ -16,6 +16,19 @@ function seleccionar() {
   menuVisible = false;
 }
 
+// Función para cambiar la visibilidad al hacer clic en el h2
+function mostrarElementos(idElemento) {
+  var elementosMostrables = document.getElementById(idElemento);
+
+  // Alternar la visibilidad y la opacidad para mostrar u ocultar los elementos
+  if (elementosMostrables.style.visibility === "hidden") {
+    elementosMostrables.style.visibility = "visible";
+    elementosMostrables.style.opacity = 1;
+  } else {
+    elementosMostrables.style.visibility = "hidden";
+    elementosMostrables.style.opacity = 0;
+  }
+}
 //detecto el scrolling para aplicar la animacion de la barra de habilidades
 // window.onscroll = function(){
 //     efectoHabilidades();
